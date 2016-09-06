b16646ddd430b60fd0aa16dbee3833a94260b8f9
Emmanuel <emmanueljucaban@gmail.com>
2016-09-05 21:36:21 -0700

    Fixed tests

--- app/tests/api/TodoAPI.test.jsx
+++ app/tests/api/TodoAPI.test.jsx
@@ -80,13 +80,18 @@ describe('TodoAPI', () => {
     });
 
     it('should sort by showCompleted status', () => {
-      var filteredTodos = TodoAPI.filterTodos(todos, true, 'some');
-      expect(filteredTodos.length).toBe(2);
+      var filteredTodos = TodoAPI.filterTodos(todos, true, '');
+      expect(filteredTodos[0].completed).toBe(false);
     });
 
     it('should filter todos by searchText', () => {
-      var filteredTodos = TodoAPI.filterTodos(todos, true, '');
-
+      var filteredTodos = TodoAPI.filterTodos(todos, true, 'some');
+      expect(filteredTodos.length).toBe(2);
     })
+
+    it('should return all todos if searchText is empty', () => {
+      var filteredTodos = TodoAPI.filterTodos(todos, true, '');
+      expect(filteredTodos.length).toBe(3);
+    });
   });
 });
