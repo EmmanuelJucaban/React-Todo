39b254730b1c24a25ab38e3186fa060a9670678c
Emmanuel <emmanueljucaban@gmail.com>
2016-09-22 14:38:07 -0700

    Add actions and action tests

---
+++ app/tests/actions/actions.test.jsx
@@ -0,0 +1,44 @@
+var expect = require('expect');
+var actions = require('actions');
+
+describe('Actions', () => {
+  it('should generate searchText action', () => {
+    var action = {
+      type: 'SET_SEARCH_TEXT',
+      searchText: 'Dog'
+    };
+    var res = actions.setSearchText(action.searchText);
+
+    expect(res).toEqual(action);
+  });
+
+  it('should generate addTodo action', () => {
+    var action = {
+      type: 'ADD_TODO',
+      todo: 'Run'
+    };
+    var res = actions.addTodo('Run');
+
+    expect(res).toEqual(action);
+  });
+
+  it('should toggle show completed', () => {
+    var action = {
+      type: 'TOGGLE_SHOW_COMPLETED'
+    };
+    var res = actions.toggleShowCompleted();
+
+    expect(res).toEqual(action);
+  });
+
+  it('should toggle todo', () => {
+    var action = {
+      type: 'TOGGLE_TODO',
+      id: 1
+    };
+    var res = actions.toggleTodo(action.id);
+    
+    expect(res).toEqual(action);
+  });
+
+});
--- app/tests/components/TodoList.test.jsx
+++ app/tests/components/TodoList.test.jsx
@@ -32,8 +32,8 @@ describe('TodoList', () => {
   it('should render empty message if no todos', () => {
     var todos = [];
     var todoList = TestUtils.renderIntoDocument(<TodoList todos={todos}/>);
-    var $el = $(ReactDOM.findDOMNode(todolist));
+    var $el = $(ReactDOM.findDOMNode(todoList));
 
-    expect($el.find('container__message').length).toBe(1);
+    expect($el.find('.container__message').length).toBe(1);
   });
 });
